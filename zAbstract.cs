f82cb1991b381f22d45e02f244d74393b900b368
Pete Beardmore <pete.beardmore@msn.com>
2018-05-20 08:01:37 +0100

    [fix] abstract, class definition syntax

--- zAbstract.cs
+++ zAbstract.cs
@@ -1,12 +1,12 @@
 
-public abstract class test() {
+public abstract class test {
     static int l = 11;
     public static int get_l(int j) {
         return (++j+l);
     }
 }
 
-public class P() {
+public class P {
     public int l;
     public void set_l(int j) {
         l = test.get_l(j);
